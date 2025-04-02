96768a1a74c5ce94b9e78c2e91bc9f35ba77c32b
Dominik Andritsch <57039264+raulduke9119@users.noreply.github.com>
2025-04-02 16:53:41 +0200

    Add package __init__.py

---
+++ chart_pattern_analyzer/__init__.py
@@ -0,0 +1,14 @@
+"""
+Chart Pattern Analyzer
+A comprehensive trading analysis application that uses computer vision 
+and machine learning to detect optimal trading patterns in financial charts.
+"""
+
+from .chart_analyzer import ChartAnalyzer, ChartPatternDatabase, ChartImageProcessor, ChartPatternModel, AlertManager
+from .visualization import ChartAnnotator, ChartVisualizer
+from .binance_data_provider import BinanceDataProvider
+from .openrouter_vision_model import OpenRouterVisionAnalyzer
+from .live_market_analyzer import LiveMarketAnalyzer
+
+__version__ = "0.1.0"
+__author__ = "Chart Pattern Analyzer Team"
